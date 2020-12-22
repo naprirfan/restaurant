c9db3b7ea4e2e0508d4c12584faaac88e16d78ac
M Irfan <naprirfan@gmail.com>
2020-12-22 16:43:15 +0100

    Extract hook logic

---
+++ src/hooks/useResults.js
@@ -0,0 +1,31 @@
+import { useEffect, useState} from 'react';
+import yelp from '../api/yelp';
+
+const DEFAULT_SEARCH_TERM = 'pasta';
+
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          term: searchTerm,
+          limit: 50,
+          location: 'Munich'
+        }
+      });
+      setErrorMessage('');
+      setResults(response.data.businesses);
+    } catch (error) {
+      setErrorMessage('Something went wrong');
+    }
+  }
+
+  useEffect(() => {
+    searchApi(DEFAULT_SEARCH_TERM)
+  }, [])
+
+  return [searchApi, results, errorMessage];
+}
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,33 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
 
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          term: searchTerm,
-          limit: 50,
-          location: 'Munich'
-        }
-      });
-      setErrorMessage('');
-      setResults(response.data.businesses);
-    } catch (error) {
-      setErrorMessage('Something went wrong');
-    }
-  }
-
-  useEffect(() => {
-    searchApi('pasta')
-  }, [])
-  
   return (
     <View>
         <SearchBar 
