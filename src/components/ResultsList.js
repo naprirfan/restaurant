0c3434eda2670305e8c552144141a9f88856aace
M Irfan <naprirfan@gmail.com>
2020-12-24 11:32:43 +0100

    Add withNavigation and remove props drilling

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
 import ResultsCard from './ResultsCard';
 
 const ResultsList = ({ title, results, navigation }) => {
@@ -37,4 +38,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
